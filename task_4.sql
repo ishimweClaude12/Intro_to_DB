07da19dc14f2b6a09e1569ac4376e5a2c5f80805
Claude Ishimwe <dpqb12haikuo@gmail.com>
2025-10-14 17:39:46 +0200

    revert: back to the first passing test

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,5 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Print the full description of the books table without using DESCRIBE or EXPLAIN
-SELECT COLUMN_NAME, COLUMN_TYPE 
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = 'alx_book_store' 
-AND TABLE_NAME = 'Books';
+-- Print the full description of the books table without using EXPLAIN
+SHOW CREATE TABLE books;
