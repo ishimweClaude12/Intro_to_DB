fd29a1a7a408b0955ebbd3f5275af7ea00706336
Claude Ishimwe <dpqb12haikuo@gmail.com>
2025-10-14 17:37:36 +0200

    fix: failing second test

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,8 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Print the full description of the books table without using EXPLAIN
-SHOW CREATE TABLE books;
+-- Print the full description of the books table without using DESCRIBE or EXPLAIN
+SELECT COLUMN_NAME, COLUMN_TYPE 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'Books';
