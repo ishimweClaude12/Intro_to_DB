43a6b8e4b3a56e271ad7ad76f6d1a1ca6e28e9f7
Claude Ishimwe <dpqb12haikuo@gmail.com>
2025-10-14 17:22:08 +0200

    task2

---
+++ task_2.sql
@@ -0,0 +1,43 @@
+USE alx_book_store;
+
+-- 1. AUTHORS table
+CREATE TABLE IF NOT EXISTS AUTHORS (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- 2. BOOKS table
+CREATE TABLE IF NOT EXISTS BOOKS (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+);
+
+-- 3. CUSTOMERS table
+CREATE TABLE IF NOT EXISTS CUSTOMERS (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+-- 4. ORDERS table
+CREATE TABLE IF NOT EXISTS ORDERS (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+);
+
+-- 5. ORDER_DETAILS table
+CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+);
