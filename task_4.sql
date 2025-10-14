fd05c785c243b89c650de7889e36eba1fa5f0054
Claude Ishimwe <dpqb12haikuo@gmail.com>
2025-10-14 17:32:28 +0200

    fix: task 4 all failing tests

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Print the full description of the books table without using DESCRIBE or EXPLAIN
-SHOW CREATE TABLE books;
+-- Print the full description of the books table without using   EXPLAIN
+DESCRIBE books;
