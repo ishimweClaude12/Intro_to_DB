429e3db82144a0db26c887a74c6887e78dcb9bcc
Claude Ishimwe <dpqb12haikuo@gmail.com>
2025-10-14 17:25:03 +0200

    rename: all tables

--- task_2.sql
+++ task_2.sql
@@ -1,23 +1,23 @@
 USE alx_book_store;
 
 -- 1. AUTHORS table
-CREATE TABLE AUTHORS (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- 2. BOOKS table
-CREATE TABLE BOOKS (
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 -- 3. CUSTOMERS table
-CREATE TABLE CUSTOMERS (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
@@ -25,19 +25,19 @@ CREATE TABLE CUSTOMERS (
 );
 
 -- 4. ORDERS table
-CREATE TABLE ORDERS (
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- 5. ORDER_DETAILS table
-CREATE TABLE ORDER_DETAILS (
+CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
